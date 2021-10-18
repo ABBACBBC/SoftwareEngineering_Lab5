0b6a26a4bc4d8461abed1c1099c1e17d3525558f
233zzy233 <923523891@qq.com>
2021-10-18 16:50:42 +0800

    张智洋的提交

--- src/Lab5/calculation.java
+++ src/Lab5/calculation.java
@@ -6,5 +6,21 @@ public class calculation {
 	}
 	
 	
+	public double counter() {
+		int first=(int)(Math.random()*100);
+		int second=(int)(Math.random()*100);
+		int sym=(int)(Math.random());
+		int result = 0;
+		if(sym==0) {
+			result=first+second;
+			System.out.println(first+"+"+second+"=?");
+		}
+		else if(sym==1){
+			result=first-second;
+			System.out.println(first+"-"+second+"=?");
+		}
+		double r=(double)result;
+		return r;
 
+		}
 }
